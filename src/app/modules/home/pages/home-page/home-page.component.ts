cd5a7d3bcb3b89a8bbb3f01bf45d9043dd7b2efa
Luis Antonio Vázquez García <luis.vazquezga@udlap.mx>
2023-09-23 22:52:33 -0600

    adding pages to modules

--- src/app/modules/home/pages/home-page/home-page.component.ts
+++ src/app/modules/home/pages/home-page/home-page.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home-page',
-  templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  selector: 'app-home-page',  //Lo que se usa para Llamar componente dentro de html
+  templateUrl: './home-page.component.html',  //html asociado a ese componente
+  styleUrls: ['./home-page.component.css']  //estilos del componente
 })
 export class HomePageComponent {
 
--- src/app/shared/components/side-bar/side-bar.component.ts
+++ src/app/shared/components/side-bar/side-bar.component.ts
@@ -6,5 +6,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./side-bar.component.css']
 })
 export class SideBarComponent {
+  mainMenu: {
+    defaultOptions: Array<any>,
+    accessLink: Array<any>
+  } = { defaultOptions: [], accessLink: [] };
 
+  customOptions: Array<any> = [];
+
+  ngOnInit(): void {
+    this.mainMenu.defaultOptions = [
+      {
+        name: 'Home',
+        icon: 'uil uil-estate',
+        router: ['/']
+      },
+      {
+        name: 'Buscar',
+        icon: 'uil uil-search',
+        router: ['/', 'history']
+      },
+      {
+        name: 'Tu biblioteca',
+        icon: 'uil uil-chart',
+        router: ['/', 'favorites']
+      },
+    ]
+
+    this.mainMenu.accessLink = [
+      {
+        name: 'Crear Lista',
+        icon: 'uil-plus-square'
+      },
+      {
+        name: 'Canciones que te gustan',
+        icon: 'uil-heart-medical'
+      },
+    ]
+
+    this.customOptions = [
+      {
+        name: 'Lista 1',
+        router: ['/']
+      },
+      {
+        name: 'Lista 2',
+        router: ['/']
+      }
+    ]
+  }
 }
