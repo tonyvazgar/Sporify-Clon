7a2ccd1335ddf1ad4ee7b7e1e7ec11051481941b
Luis Antonio Vázquez García <luis.vazquezga@udlap.mx>
2023-09-23 19:37:55 -0600

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeModule } from './modules/home/home.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./modules/home/home.module').then( module => module.HomeModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
