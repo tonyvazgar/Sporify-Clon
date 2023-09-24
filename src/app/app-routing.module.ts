e36184e25585f17806eca66a42331a77029cfdc6
Luis Antonio Vázquez García <luis.vazquezga@udlap.mx>
2023-09-24 00:03:16 -0600

    adding pages to modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeModule } from './modules/home/home.module';
+import { HomePageComponent } from '@modules/home/pages/home-page/home-page.component';
 
 const routes: Routes = [
   {
+    path: 'auth',
+    loadChildren: () => import('./modules/auth/auth.module').then( module => module.AuthModule)
+  },
+  {
     path: '',
+    component: HomePageComponent,
     loadChildren: () => import('./modules/home/home.module').then( module => module.HomeModule)
   }
 ];
