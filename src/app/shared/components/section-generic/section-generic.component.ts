52f0c8d011eef0d3b0d8745e01844856f4811757
Luis Antonio Vázquez García <luis.vazquezga@udlap.mx>
2023-09-24 00:05:32 -0600

    adding components to shared (card and mediaplayer)

---
+++ src/app/shared/components/section-generic/section-generic.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section-generic',
+  templateUrl: './section-generic.component.html',
+  styleUrls: ['./section-generic.component.css']
+})
+export class SectionGenericComponent {
+
+}
