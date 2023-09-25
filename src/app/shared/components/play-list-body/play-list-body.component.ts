44ef6dc5bf5fff8749b5ac7d4c5b6fb43c1d67f3
Luis Antonio Vázquez García <luis.vazquezga@udlap.mx>
2023-09-24 22:05:47 -0600

    implementing OrderListPipe

--- src/app/shared/components/play-list-body/play-list-body.component.ts
+++ src/app/shared/components/play-list-body/play-list-body.component.ts
@@ -4,13 +4,25 @@ import * as dataRaw from '../../../data/tracks.json';
 @Component({
   selector: 'app-play-list-body',
   templateUrl: './play-list-body.component.html',
-  styleUrls: ['./play-list-body.component.css']
+  styleUrls: ['./play-list-body.component.css'],
 })
 export class PlayListBodyComponent {
   @Input() tracks: Array<any> = [];
-
+  optionSort: { property: string | null; order: string } = {
+    property: null,
+    order: 'asc',
+  };
   ngOnInit(): void {
-    const {data} = (dataRaw as any).default;
+    const { data } = (dataRaw as any).default;
     this.tracks = data;
+  }
+
+  changeOrder(property: string): void {
+    const { order } = this.optionSort;
+    this.optionSort = {
+      property,
+      order: order === 'asc' ? 'desc' : 'asc'
+    }
+    console.log(this.optionSort);
   }
 }
