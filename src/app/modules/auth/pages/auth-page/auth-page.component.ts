8555d709acece803b616997167614b5f4342a17f
Luis Antonio Vázquez García <luis.vazquezga@udlap.mx>
2023-09-26 20:15:42 -0600

    implementing formgroup

--- src/app/modules/auth/pages/auth-page/auth-page.component.ts
+++ src/app/modules/auth/pages/auth-page/auth-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth-page',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./auth-page.component.css']
 })
 export class AuthPageComponent {
+  formLogin: FormGroup = new FormGroup({});
+  @Input() errorSession: boolean = false;
 
+  sendLogin(): void {
+    const body = this.formLogin.value;
+
+    console.log(body);
+  }
+
+  ngOnInit(): void {
+    this.formLogin = new FormGroup(
+      {
+        email: new FormControl('', [
+          Validators.required,
+          Validators.email
+        ]),
+        password: new FormControl('', [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(10)
+        ])
+      }
+    )
+  }
 }
