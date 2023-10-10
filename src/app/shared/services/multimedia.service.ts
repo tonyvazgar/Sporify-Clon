5c4279f37f392327c642b5f46fb6a95ae344cd45
Luis Antonio Vázquez García <luis.vazquezga@udlap.mx>
2023-10-09 23:32:37 -0600

    confugurando tiempos

--- src/app/shared/services/multimedia.service.ts
+++ src/app/shared/services/multimedia.service.ts
@@ -11,6 +11,8 @@ export class MultimediaService {
   public trackInfo$: BehaviorSubject<any> = new BehaviorSubject(undefined);
   public audio!: HTMLAudioElement
   myObservable$: Observable<any> = new Observable();
+  public timeElapsed$: BehaviorSubject<string> = new BehaviorSubject('00:00');
+  public timeRemaining$: BehaviorSubject<string> = new BehaviorSubject('-00:00');
   constructor() {
     this.audio = new Audio();
     this.trackInfo$.subscribe(responseOk => {
@@ -18,10 +20,39 @@ export class MultimediaService {
         this.setAudio(responseOk);
       }
     })
+    this.listenAllEvents();
   }
 
-  public listenAllElements(): void {
+  private listenAllEvents(): void {
+    this.audio.addEventListener('timeupdate', this.calculateTime, false)
+  }
 
+  private calculateTime = () => {
+    console.log('Disparando evento de calcular')
+    const { duration, currentTime } = this.audio;
+    console.table([duration, currentTime])
+    this.setElapsedTime(currentTime);
+    this.setRemaining(currentTime, duration);
+  }
+
+  private setElapsedTime(currentTime: number): void {
+    let seconds = Math.floor(currentTime % 60) //TODO 1,2,3
+    let minutes = Math.floor((currentTime / 60) % 60)
+    //TODO  00:00 ---> 01:05 --> 10:15
+    const displaySeconds = (seconds < 10) ? `0${seconds}` : seconds;
+    const displayMinutes = (minutes < 10) ? `0${minutes}` : minutes;
+    const displayFormat = `${displayMinutes}:${displaySeconds}`
+    this.timeElapsed$.next(displayFormat)
+  }
+
+  private setRemaining(currentTime: number, duration: number): void {
+    let timeLeft = duration - currentTime;
+    let seconds = Math.floor(timeLeft % 60)
+    let minutes = Math.floor((timeLeft / 60) % 60)
+    const displaySeconds = (seconds < 10) ? `0${seconds}` : seconds;
+    const displayMinutes = (minutes < 10) ? `0${minutes}` : minutes;
+    const displayFormat = `-${displayMinutes}:${displaySeconds}`
+    this.timeRemaining$.next(displayFormat)
   }
 
   public setAudio(song: TrackModel): void {
